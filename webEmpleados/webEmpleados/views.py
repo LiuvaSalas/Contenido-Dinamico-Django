e15f97612f736942aa553a37bded4a7b99ce4190
LiuvaSalas <liuvasalas01@gmail.com>
2024-10-14 22:00:40 -0300

    xx

--- webEmpleados/webEmpleados/views.py
+++ webEmpleados/webEmpleados/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
-def mostrandoContenidoDinamico(request):
-    empleados = ["Liuva Salas", "Matias Fuentes", "Isis Gonzalez", "Monica Candia", "Luval Salas", "Diorlette Perez"]
-    context = {"empleados" : empleados}
-    return render(request, "index.html", context)
+# def mostrandoContenidoDinamico(request):
+#    empleados = ["Liuva Salas", "Matias Fuentes", "Isis Gonzalez", "Monica Candia", "Luval Salas", "Diorlette Perez"]
+#   context = {"empleados" : empleados}
+#    return render(request, "index.html", context)
+
+
+def plantilla(request):
+    return render(request, "index.html ", {})
+
+
+def herencia1(request):
+    return render(request, "herencia1.html", {})
